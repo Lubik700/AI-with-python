f657f013ab114f352713468b9029c84e6ae02db5
Lubik <lubiknewa7@gmail.com.com>
2025-05-31 18:14:56 +0545

    Week 1

---
+++ Week 1/Day 1/Day1.py
@@ -0,0 +1,31 @@
+# age=27
+# is_hot=True
+# print(age)
+# print(is_hot)
+# print(type(age))
+
+
+# age=26
+# print("Ram is "+str(age)+" years old")
+# num=26
+# b="Ram"
+# c=True
+# d=3.66
+# print(num)
+# print(b)
+# print(c)
+# print(d)
+
+# num=int(input("Enter a number\n"))
+# if(num>0):
+#   print("positive")
+# elif(num<0):
+# print("negative")
+# else:
+# print("zero")
+
+# for i in range(1,11):
+#   print(i)
+
+for i in range(1,11):
+ print(i*i)
